09394790136a077b493406008ef850150a9f0f9d
Salim Abdu <salimabdu774@gmail.com>
2024-07-19 19:45:55 +0300

    creating playRound function

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ function getHumanChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   }
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    console.log(`Human choice: ${humanChoice}`);
+    console.log(`Computer choice: ${computerChoice}`);
+  
+    if (humanChoice === computerChoice) {
+      console.log("It's a tie!");
+      return "tie";
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+      console.log(
+        `You win! ${
+          humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+        } beats ${
+          computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+        }`
+      );
+      return "human";
+    } else {
+      console.log(
+        `You lose! ${
+          computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+        } beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`
+      );
+      return "computer";
+    }
+  }
+  
